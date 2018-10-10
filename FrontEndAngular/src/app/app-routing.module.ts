e1d0c35a74ed6c1645aea7e00870983b549ddcc6
William Baquero <williambaqueror@gmail.com>
2018-10-10 17:48:15 -0500

    Login

--- FrontEndAngular/src/app/app-routing.module.ts
+++ FrontEndAngular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InventarioComponent } from './inventario/inventario.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inventario/', component: InventarioComponent},
+  {path: 'login/', component:LoginComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'login/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEndAngular/src/app/app.module.ts
+++ FrontEndAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventarioComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
