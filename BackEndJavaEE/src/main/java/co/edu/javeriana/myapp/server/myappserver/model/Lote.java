17357159530daccb63743b2a3e033a6a88a5f836
William Baquero <williambaqueror@gmail.com>
2018-10-10 20:44:43 -0500

    entidades 1.2

--- BackEndJavaEE/src/main/java/co/edu/javeriana/myapp/server/myappserver/model/Lote.java
+++ BackEndJavaEE/src/main/java/co/edu/javeriana/myapp/server/myappserver/model/Lote.java
@@ -1,12 +1,19 @@
 package co.edu.javeriana.myapp.server.myappserver.model;
  
 import java.sql.Date;
- 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
  
+@Entity
 public class Lote{
-    @ManyToOne 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String codigoSKU;
+    @ManyToOne 
     private Producto producto;
     private int precio;
     private int stock;
